41eab6c08dd1d59fde43af9ced2e473157c1db8b
Eugene Obrezkov <ghaiklor@gmail.com>
2021-10-15 20:56:21 +0300

    [semver]: implement increment function

--- src/semver.rs
+++ src/semver.rs
@@ -5,6 +5,12 @@ const MAX_MAJOR_VERSION: u8 = 5;
 const MAX_MINOR_VERSION: u8 = 10;
 const MAX_PATCH_VERSION: u8 = 20;
 
+pub enum SemVerSection {
+    Major,
+    Minor,
+    Patch,
+}
+
 #[derive(Clone, Copy, PartialEq)]
 pub struct SemVer {
     major: u8,
@@ -21,6 +27,16 @@ impl SemVer {
             minor: rng.gen_range(0..MAX_MINOR_VERSION),
             patch: rng.gen_range(0..MAX_PATCH_VERSION),
         }
+    }
+
+    pub fn increment(mut self, section: SemVerSection) -> Self {
+        match section {
+            SemVerSection::Major => self.major += 1,
+            SemVerSection::Minor => self.minor += 1,
+            SemVerSection::Patch => self.patch += 1,
+        }
+
+        self
     }
 }
 
@@ -41,5 +57,17 @@ mod tests {
         assert!(version.major <= MAX_MAJOR_VERSION);
         assert!(version.minor <= MAX_MINOR_VERSION);
         assert!(version.patch <= MAX_PATCH_VERSION);
+    }
+
+    #[test]
+    fn it_increments_version() {
+        let version = SemVer::generate();
+        let v0 = version.increment(SemVerSection::Patch);
+        let v1 = version.increment(SemVerSection::Minor);
+        let v2 = version.increment(SemVerSection::Major);
+
+        assert_eq!(v0.patch, version.patch + 1);
+        assert_eq!(v1.minor, version.minor + 1);
+        assert_eq!(v2.major, version.major + 1);
     }
 }
